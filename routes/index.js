e3f870c7a0530e155f53e8512a7c676ecb35daee
lfernando2091 <lfernando2091@gmail.com>
2018-11-10 16:10:56 -0600

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+const mysql  = require('mysql');
+const mybatisMapper = require('mybatis-mapper');
+var router = express.Router();
+
+/*Conecxion a MySql*/
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'LuisFernando2091',
+  database : 'practica5'
+});
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});
+
+connection.end();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Tomorrowland' });
+});
+
+module.exports = router;
